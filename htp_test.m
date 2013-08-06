4cc1cfff0504989df5637ff8138021f73cf24c9a
Akhil Kapoor <akhil.kapoor876@gmail.com>
2013-08-06 19:31:18 -0400

    Adding tests for optional param handling in htp
    These can be used for example as well

--- htp_test.m
+++ htp_test.m
@@ -2,13 +2,33 @@ vecSize     = 1000; % Size of the vector in the 'original' input space
 nbMeasures  = 200; % size of the measurement vector
 
 A = randn( nbMeasures, vecSize )/sqrt(nbMeasures);
-x = zeros(vecS, 1);
+x = zeros(vecSize, 1);
 s = 25;
 x(1:s) = rand(s,1);
 y = A*x;
+
+n = 1;
+disp(['Test ', num2str(n)]); n = n+1;
 [xStar1,~,~,~,~,~] = htp_back(y,A,s);
 res = norm(x - xStar1);
 disp(['Using original htp, the result is: ', num2str(res)]);
-[xStar2,~,~,~,~,~] = htp(y,A,s);
+
+disp(['Test ', num2str(n)]); n = n+1;
+[xStar2,~,~,~,~,~] = htp(y,A,s,{});
 res = norm(x - xStar2);
+disp(['Using modified htp, the result is: ', num2str(res)]);
+
+disp(['Test ', num2str(n)]); n = n+1;
+[xStar3,~,~,~,~,~] = htp(y,A,s,{'MaxNbIter', 500});
+res = norm(x - xStar3);
+disp(['Using modified htp, the result is: ', num2str(res)]);
+
+disp(['Test ', num2str(n)]); n = n+1;
+[xStar4,~,~,~,~,~] = htp(y,A,s,{'maxbIter', 500});
+res = norm(x - xStar4);
+disp(['Using modified htp, the result is: ', num2str(res)]);
+
+disp(['Test ', num2str(n)]); n = n+1;
+[xStar5,~,~,~,~,~] = htp(y,A,s,{'maxNbIter'});
+res = norm(x - xStar5);
 disp(['Using modified htp, the result is: ', num2str(res)]);
