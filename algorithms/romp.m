0a575b851e7e01ab0234a0561578e4969c2cd50e
Akhil Kapoor <akhil.kapoor876@gmail.com>
2013-08-20 00:58:58 -0400

    Adding more control variables
    1) Adding sparsity as one of required function parameters.
    2) Allowing user to add tolerance residual
    3) Updating appropriate comments

--- algorithms/romp.m
+++ algorithms/romp.m
@@ -1,30 +1,29 @@
-function [x, NbIter] = romp(y, A, varargin)
+function [x, NbIter] = romp(y, A, s, varargin)
 
-    % So make the varargin check for only MaxNbIter
     if size(varargin, 2)
         if iscell(varargin{1})
-            [checkArgs, MaxNbIter] = parse_Varargin(varargin{1});
+            [checkArgs, MaxNbIter, TolRes] = parse_Varargin(varargin{1});
         else
-            disp('USAGE: ''MaxNbIter'', value expected in a cell.');
+            disp('USAGE: ''parameter_name'', paramenter_value pairs expected in a cell.');
             disp(['Ignoring arguments: ', varargin]);
-            [~, MaxNbIter] = parse_Varargin({});
+            [~, MaxNbIter, TolRes] = parse_Varargin({});
             checkArgs = false;
         end
-        
     else
-        [checkArgs, MaxNbIter] = parse_Varargin({});
+        [checkArgs, MaxNbIter, TolRes] = parse_Varargin({});
     end
     
     if ~checkArgs
         disp('WARNING:^');
     end
     
-    [x, NbIter] = romp_Vershynin(MaxNbIter, A, y);
+    [x, NbIter] = romp_Vershynin(s, A, y, MaxNbIter, TolRes);
 end
 
-function [checkArgs, MaxNbIter] = parse_Varargin(optional_param)
+function [checkArgs, MaxNbIter, TolRes] = parse_Varargin(optional_param)
 
     MaxNbIter = 500;
+    TolRes = 1e-4;
 
     [~, s] = size(optional_param);
     if rem(s, 2) ~= 0
@@ -32,12 +31,14 @@ function [checkArgs, MaxNbIter] = parse_Varargin(optional_param)
         disp('Variable argument list is incomplete.');
         disp('Given arguments:');
         disp(optional_param{:});
-        disp('USAGE: ''MaxNbIter'', value expected.');
+        disp('USAGE: ''parameter_name'', parameter_value pairs expected in a cell.');
     else
         checkArgs = true;
         for i = 1:2:s
             if strcmpi(optional_param{i}, 'MaxNbIter')
                 MaxNbIter = optional_param{i+1};
+            elseif strcmpi(optional_param{i}, 'TolRes')
+                TolRes = optional_param{i+1};
             else
                 checkArgs = false;
                 disp(['Unexpected optional parameter: ', optional_param{i}, '. ', mat2str(optional_param{i+1}), ' is being ignored.']);
